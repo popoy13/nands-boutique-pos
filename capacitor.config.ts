a1008672f55e914987d4696a373525d76c6e2149
popoy13 <popoy13@users.noreply.github.com>
2026-09-16 13:55:12 +0700

    feat: pratinjau struk di setelan printer

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.nandsboutique.pos',
-  appName: "Nand's Boutique POS",
-  webDir: 'dist'
-};
-
-export default config;
